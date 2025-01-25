3040460004ea8750177ef23e1f23829f7298111f
arvin <amirsaafari10@gmail.com>
2025-01-25 23:19:52 +0300

    header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,36 +4,41 @@ import MenuItem from "../components/MenuItem";
 
 function Header() {
     return (
-        <header>
-            <div>
-                <div>
-                    <button>
-                        <img src="/images/location.svg" alt="location" />
-                        <p>
-                            تهران دانشگاه تهران کارگرشما...
-                        </p>
-                        <img src="/images/bottom-purple.svg" alt="arrow" />
-                    </button>
-
-                    <div>
+        <header className="p-2 shadow-md">
+            <div className=" flex p-4 justify-between" > 
+                
+                    <div className=" flex items-center space-x-8 ">
                         <button>
-                            <img src="/images/order.svg" alt="order" />
+                            <img src="/images/order.svg" alt="order" className="w-5 h-5" />
                         </button>
                         <button>
-                            <img src="/images/user.svg" alt="user" />
+                            <img src="/images/user.svg" alt="user" className="w-5 h-5" />
                         </button>
                         <button>
-                            <img src="/images/search.svg" alt="search" />
+                            <img src="/images/search.svg" alt="search" className="w-5 h-5"/>
                         </button>
                     </div>
-                </div>
 
-                <div>
-                    {menuitem.map((item, id) => (
+               
+                    <div className="flex items-center space-x-2" >
+                        <img src="/images/bottom-purple.svg" alt="arrow" className="w-3 h-3"/>
+                        <p className=" text-xs text-gray-500">
+                            تهران دانشگاه تهران کارگرشما
+                        </p>
+                        <img src="/images/location.svg" alt="location"/>
+                    </div>  
+                
+                
+            </div>
+
+            <div className="p-4">
+                <div className="flex overflow-x-auto space-x-10 scroll-smooth">
+                    {menuitem.reverse().map((item, id) => (
                         <MenuItem key={id} icon={item.icon} name={item.title} />
                     ))}
                 </div>
             </div>
+                
         </header>
     );
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -8,9 +8,9 @@ interface MenuItemProps {
 
 function MenuItem ({icon, name}: MenuItemProps): JSX.Element {
     return (
-        <div>
-            <img src={icon} alt={name} />
-            <span> {name} </span>
+        <div className="flex-none flex flex-col items-center justify-center space-y-4">
+            <img className="w-10 h-10 object-contain" src={icon} alt={name} />
+            <span className="text-[0.7rem] text-center whitespace-nowrap"> {name} </span>
         </div>
     );
 }
